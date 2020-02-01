93e70b80ab539f502ec69f1aebd7c66d3bb18189
RomainHerault <heraultromain8@gmail.com>
2020-02-01 17:17:49 +0100

    Début des tests rayonView

--- StoreManager/views.py
+++ StoreManager/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-from StoreManager.models import Product
+from StoreManager.models import Product, Department, User
 from StoreManager.models import Employee
 from . import forms
 
@@ -40,11 +40,11 @@ def rayon(request):
 
         for product_id in selected_products_id:
 
-            product = Product.objects.get(pk = product_id)
-            if product.department.id is not request.user.employee.department.id :
+            product = Product.objects.get(pk=product_id)
+            if product.department.id is not request.user.employee.department.id:
                 error = True,
                 error_message = "Vous ne pouvez pas modifier tous les produits séléctionnés (mauvais département)"
-            else :
+            else:
                 product.name = name_products[id_products.index(int(product_id))]
                 product.price = price_products[id_products.index(int(product_id))]
                 product.quantity = quantity_products[id_products.index(int(product_id))]
@@ -69,47 +69,59 @@ def rayon(request):
     header = ['Action', 'Nom', 'Prix', 'Quantité', 'Ref', 'Nom Rayon']
     query_results = Product.objects.all()  # TODO à modifier en fonction des droits du user
     return render(request, 'StoreManager/base.html',
-                  {'username': request.user.username, 'header': header, 'data': query_results, 'error': error, 'error_message' : error_message})
+                  {'username': request.user.username, 'header': header, 'data': query_results, 'error': error,
+                   'error_message': error_message})
 
 
 def departement(request):
-    if request.method == 'POST' and request.POST.get('add_product'):  # check if post request comes from correct button
-        new_product = Product(name="", quantity=0, ref="", price=0,
-                              department_id=1)  # TODO dept id à modifier en fonction des droits du user
-        new_product.save()
+    if request.method == 'POST' and request.POST.get('add_dep'):  # check if post request comes from correct button
+        new_dep = Department(name="", store_id=1)
+        new_dep.save()
     elif request.method == 'POST' and request.POST.get(
-            'modify_product'):  # check if post request comes from correct button
+            'modify_dep'):  # check if post request comes from correct button
 
-        selected_products_id = request.POST.getlist('action_product')
+        selected_dep_id = request.POST.getlist('action_dep')
 
-        id_products = list(Product.objects.values_list('id', flat=True))
-        id_products.sort()
+        id_deps = list(Department.objects.values_list('id', flat=True))
+        id_deps.sort()
 
-        name_products = request.POST.getlist('name_product')
-        price_products = request.POST.getlist('price_product')
-        quantity_products = request.POST.getlist('quantity_product')
-        ref_products = request.POST.getlist('ref_product')
+        dep_name = request.POST.getlist('dep_name')
+        username = request.POST.getlist('username')
 
-        for product_id in selected_products_id:
+        for dep_id in selected_dep_id:
+            dep = Department.objects.get(pk=dep_id)
+            dep.name = dep_name[id_deps.index(int(dep_id))] # TODO check if dep name already exist
+            dep.save()
 
-            product = Product.objects.get(pk = product_id)
+            if username[id_deps.index(int(dep_id))]:
+                user = User.objects.get(username=username[id_deps.index(int(dep_id))]) # todo check if user not already assigned or does not exist
+                employee = Employee.objects.get(user=user)
+                employee.department = dep
+                employee.save()
 
-            product.name = name_products[id_products.index(int(product_id))]
-            product.price = price_products[id_products.index(int(product_id))]
-            product.quantity = quantity_products[id_products.index(int(product_id))]
-            product.ref = ref_products[id_products.index(int(product_id))]
-            product.save()
 
     elif request.method == 'POST' and request.POST.get(
-            'delete_product'):  # check if post request comes from correct button
+            'delete_dep'):  # check if post request comes from correct button
 
-        selected_products = request.POST.getlist('action_product')  # get id of selected products
-        Product.objects.filter(id__in=selected_products).delete()  # delete selected product
+        selected_dep = request.POST.getlist('action_dep')  # get id of selected products
+        Department.objects.filter(id__in=selected_dep).delete()  # delete selected product
 
-    header = ['Action', 'Nom', 'Prix', 'Quantité', 'Ref', 'Nom Rayon']
-    query_results = Product.objects.all()  # TODO à modifier en fonction des droits du user
+    header = ['Action', 'Departement', 'Username']
+    query_employee_results = Employee.objects.all().select_related()
+    query_dept = Department.objects.all()
+
+    custom_data = []
+
+    for dept in query_dept:
+        for emp in query_employee_results:
+            if dept.name == emp.department.name:
+                custom_data.append([dept, emp])
+        if custom_data[-1][0].name != dept.name:
+            custom_data.append([dept])
+
     return render(request, 'StoreManager/departement.html',
-                  {'username': 'Jean Michel', 'header': header, 'data': query_results})
+                  {'username': 'Jean Michel', 'header': header, 'data': custom_data})
+
 
 def connexion(request):
     error = False
@@ -123,7 +135,7 @@ def connexion(request):
                 login(request, user)  # nous connectons l'utilisateur
                 if 'next' in request.GET:
                     next = request.GET['next']
-                    if next is not None :
+                    if next is not None:
                         return redirect(next)
             else:  # sinon une erreur sera affichée
                 error = True
